f2a90b9840ebe709e528f2bbf32d3dfc418a35f7
Rei Schädeli <rei@fae.red>
2025-12-04 10:22:25 +0100

    fix: better token detection

--- db-service/src/main/java/ch/semafor/intens/ws/service/BaseServiceImpl.java
+++ db-service/src/main/java/ch/semafor/intens/ws/service/BaseServiceImpl.java
@@ -408,7 +408,7 @@ public class BaseServiceImpl {
 
         // oauth2 mode
         Map<String, Object> principalAttrs = SecurityUtils.getPrincipalAttributes();
-        if( principalAttrs.containsKey(properties.getOauthUserField())){
+        if( principalAttrs.containsKey("given_name") || principalAttrs.containsKey(properties.getOauthUserField())){
             Collection<SimpleGrantedAuthority> authorities = (Collection<SimpleGrantedAuthority>)
                 SecurityContextHolder.getContext().getAuthentication().getAuthorities();
             Owner currentUser = new Owner((String) principalAttrs.get(properties.getOauthUserField()));
